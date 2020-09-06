43cb73bec038bbf9c56643eb5070d0f42863efc3
axelangelon <70852972+axelangelon@users.noreply.github.com>
2020-09-06 17:43:30 +0200

    Add files via upload

---
+++ L2 #1.py
@@ -0,0 +1,5 @@
+print("This program will convert a distance in kilometres to miles")
+a = int(input("Please input the distance in km: "))
+conversion_constant = 0.6214
+miles = a*conversion_constant
+print("The distance in kilometres is equal to", miles, "miles")
---
+++ L2 #2.py
@@ -0,0 +1,6 @@
+antal_a = int(input("Hur många av typ A platserna har sålts? "))
+antal_b = int(input("Hur många av typ B platserna har sålts? "))
+antal_c = int(input("Hur många av typ C platserna har sålts? "))
+tot_summa = antal_a*290 + antal_b*220 + antal_c*140
+print("Det har sålts totalt för totalt", tot_summa , "kronor")
+
